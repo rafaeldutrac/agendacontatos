f9ec59e81afb5214eabc2d8926a686b6c63a26b0
rafaeldutrac <rafaeldutrac@hotmail.com>
2022-10-31 12:44:25 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/entities/Contato.java
@@ -0,0 +1,28 @@
+package br.com.cotiinformatica.entities;
+
+import java.sql.Date;
+
+import br.com.cotiinformatica.enums.TipoContato;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Contato {
+	
+	private Integer idcontato;
+	private String nome;
+	private String telefone;
+	private String email;
+	private Date dataNascimento;
+	private TipoContato tipo;
+	private Usuario usuario;
+
+}
